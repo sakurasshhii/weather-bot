978f8e5816da19554b8563cb2292e766b870ab4f
Arina <skyliayandex.ru@gmail.com>
2026-01-12 15:15:10 +0300

    router command_start added

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from config_data.config import Config, load_config
 
+import app.bot.handlers
+
 
 async def main() -> None:
     config: Config = load_config()
 
     bot = Bot(token=config.tg_bot.bot_token)
     dp = Dispatcher()
+
+    dp.include_router(app.bot.handlers.start_router)
     
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
