d44b78fbd2f708c35b48def6114b42e161f62d80
Arina <skyliayandex.ru@gmail.com>
2026-01-12 21:28:28 +0300

    logging added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 
 from aiogram import Bot, Dispatcher
 from config_data.config import Config, load_config
@@ -6,7 +7,10 @@ from config_data.config import Config, load_config
 import app.bot.handlers
 
 
+logger = logging.getLogger(__name__)
+
 async def main() -> None:
+    logging.basicConfig(level=logging.DEBUG)
     config: Config = load_config()
 
     bot = Bot(token=config.tg_bot.bot_token)
@@ -18,6 +22,8 @@ async def main() -> None:
         app.bot.handlers.unexpected_router
     )
     
+    logging.info('start polling...')
+
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
