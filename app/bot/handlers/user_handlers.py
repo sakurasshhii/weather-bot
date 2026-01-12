b324e3ed90891a7f7ce310bef1ee1e1e0ba4ba48
Arina <skyliayandex.ru@gmail.com>
2026-01-12 21:13:13 +0300

    explore weather data

--- app/bot/handlers/user_handlers.py
+++ app/bot/handlers/user_handlers.py
@@ -1,14 +1,13 @@
 from aiogram.types import Message
 from aiogram import Router
 from aiogram.filters import Command
-
-import pandas as pd
-import openmeteo_requests
-
 from app.bot.lexic import coordinates
+
+import openmeteo_requests
 
 
 api_router = Router()
+
 
 @api_router.message(Command(commands=['weather']))
 async def process_weather(message: Message):
@@ -22,22 +21,26 @@ async def process_weather(message: Message):
     params = {
         "latitude": coordinates[city]["latitude"],
         "longitude": coordinates[city]["longitude"],
-        "current": ["temperature_2m", "relative_humidity_2m"],
+        "current": ["temperature_2m", "relative_humidity_2m", "precipitation", "wind_speed_10m"],
         "timezone": "auto"
     }
     responses = await openmeteo.weather_api(url, params=params)
 
-    # Process first location. Add a for-loop for multiple locations or weather models
+    # Process first location
     response = responses[0]
     print(f"Coordinates: {response.Latitude()}°N {response.Longitude()}°E")
     print(f"Elevation: {response.Elevation()} m asl")
     print(f"Timezone difference to GMT+0: {response.UtcOffsetSeconds()}s")
 
     current = response.Current()
-    current_temperature_2m = current.Variables(0).Value() # pyright: ignore[reportOptionalMemberAccess]
-    current_relative_humidity_2m = current.Variables(1).Value() # pyright: ignore[reportOptionalMemberAccess]
+    current_temperature_2m = current.Variables(0).Value()  # pyright: ignore[reportOptionalMemberAccess]
+    current_relative_humidity_2m = current.Variables(1).Value()  # pyright: ignore[reportOptionalMemberAccess]
+    current_precipitaion = current.Variables(2).Value() # pyright: ignore[reportOptionalMemberAccess]
+    current_wind_speed_10m = current.Variables(3).Value() # pyright: ignore[reportOptionalMemberAccess]
 
     await message.answer(
-        f'Current temperature: {round(current_temperature_2m, 1)}\n'\
-        f'Current relative humidity: {round(current_relative_humidity_2m, 1)}'
+        f'Current temperature: {round(current_temperature_2m, 1)}\n' \
+        f'Relative humidity: {round(current_relative_humidity_2m, 1)}\n' \
+        f'Precipitaion: {current_precipitaion}\n' \
+        f'Wind speed: {round(current_wind_speed_10m, 1)}'
     )
