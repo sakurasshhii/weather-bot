0f37e77f482940a2acb92d708c90f888600e0964
Arina <skyliayandex.ru@gmail.com>
2026-01-12 20:27:55 +0300

    handler for unknown messages

---
+++ app/bot/handlers/other_handlers.py
@@ -0,0 +1,10 @@
+from aiogram.types import Message
+from aiogram import Router
+from aiogram.filters import Command, CommandStart
+
+
+unexpected_router = Router()
+
+@unexpected_router.message()
+async def unexpected_command(message: Message):
+    await message.reply('unlknown bla bla')
