14fd29053c02a7666f9b22cf5bc0f9a04e5a5faf
Arina <skyliayandex.ru@gmail.com>
2026-01-12 21:01:37 +0300

    weather in moscow now returned

--- app/bot/handlers/user_handlers.py
+++ app/bot/handlers/user_handlers.py
@@ -13,15 +13,17 @@ api_router = Router()
 @api_router.message(Command(commands=['weather']))
 async def process_weather(message: Message):
 
+    city = 'Москва'
     openmeteo = openmeteo_requests.AsyncClient()
 
     # Make sure all required weather variables are listed here
     # The order of variables in hourly or daily is important to assign them correctly below
     url = "https://api.open-meteo.com/v1/forecast"
     params = {
-        "latitude": 55.7569,
-        "longitude": 37.6151,
-        "hourly": "temperature_2m",
+        "latitude": coordinates[city]["latitude"],
+        "longitude": coordinates[city]["longitude"],
+        "current": ["temperature_2m", "relative_humidity_2m"],
+        "timezone": "auto"
     }
     responses = await openmeteo.weather_api(url, params=params)
 
@@ -31,20 +33,11 @@ async def process_weather(message: Message):
     print(f"Elevation: {response.Elevation()} m asl")
     print(f"Timezone difference to GMT+0: {response.UtcOffsetSeconds()}s")
 
-    # Process hourly data. The order of variables needs to be the same as requested.
-    hourly = response.Hourly()
-    hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+    current = response.Current()
+    current_temperature_2m = current.Variables(0).Value() # pyright: ignore[reportOptionalMemberAccess]
+    current_relative_humidity_2m = current.Variables(1).Value() # pyright: ignore[reportOptionalMemberAccess]
 
-    hourly_data = {"date": pd.date_range(
-        start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
-        end =  pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
-        freq = pd.Timedelta(seconds = hourly.Interval()),
-        inclusive = "left"
-    )}
-
-    hourly_data["temperature_2m"] = hourly_temperature_2m
-
-    hourly_dataframe = pd.DataFrame(data = hourly_data)
-    print("\nHourly data\n", hourly_dataframe)
-
-    await message.answer('get weather')
+    await message.answer(
+        f'Current temperature: {round(current_temperature_2m, 2)}\n'\
+        f'Current relative humidity: {current_relative_humidity_2m}'
+    )
