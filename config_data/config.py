cbfc506ef00a0a3de74ae29fe32f5bada458b9e5
Arina <skyliayandex.ru@gmail.com>
2026-01-12 14:41:07 +0300

    config data file

---
+++ config_data/config.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+from environs import Env
+
+
+@dataclass
+class TgBot:
+    bot_token: str
+    admin_id: str
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+
+def load_config(path: str | None = None) -> Config:
+    env = Env()
+    env.read_env(path)
+    return Config(
+        tg_bot=TgBot(
+            bot_token=env('BOT_TOKEN'),
+            admin_id=env('ADMIN_ID')
+        )
+    )
