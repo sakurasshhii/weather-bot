a32b98ce6267be8074a8816f77feef4c58200c66
Arina <skyliayandex.ru@gmail.com>
2026-01-12 14:41:29 +0300

    config loaded, bot dtarted

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import asyncio
 
+from aiogram import Bot, Dispatcher
+from config_data.config import Config, load_config
+
 
 async def main() -> None:
-    pass
+    config: Config = load_config()
+
+    bot = Bot(token=config.tg_bot.bot_token)
+    dp = Dispatcher()
+    
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
 
 
 if __name__ == '__main__':
